b3483087215b938c6931db70614a71c7006056b4
Alain Chiasson <alain.chiasson@example.com>
2015-06-04 06:09:13 -0400

    Added deploy branch testing. Added checkout_branch

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,18 +18,33 @@ directory '/var/git_test' do
 	action :create
 end
 
-
-# Get the main branch
+# Get the master branch - description will have "master" - but branch will be deploy
 git '/var/git_test/master' do
 	repository 'https://github.com/alainchiasson/chef_git_spike'
 	reference 'master'
 	action :sync
 end
 
-# Get a Branch that does not exist
-git '/var/git_test/crap' do
+# Get the develop branch - description will have "develop" - but branch will be deploy
+git '/var/git_test/develop' do
 	repository 'https://github.com/alainchiasson/chef_git_spike'
-	reference 'crap'
+	reference 'develop'
 	action :sync
 end
+
+# Get the develop branch - description will have "develop" - but branch will be local_name
+git '/var/git_test/develop_to_local' do
+	repository 'https://github.com/alainchiasson/chef_git_spike'
+	reference 'develop'
+	checkout_branch 'local_name'
+	action :sync
+end
+
+# # Get a Branch that does not exist - thi s will fail !!
+# git '/var/git_test/crap' do
+# 	repository 'https://github.com/alainchiasson/chef_git_spike'
+# 	reference 'crap'
+# 	action :sync
+# end
+
 
