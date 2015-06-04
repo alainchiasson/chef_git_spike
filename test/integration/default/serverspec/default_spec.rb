7eab22a14ad16a1a0bca38a69c7c0b362ac98676
Alain Chiasson <alain.chiasson@example.com>
2015-06-04 07:48:12 -0400

    Added serverspec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,33 @@ require 'spec_helper'
 describe 'chef_git_spike::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  context 'using resource master' do
+
+  	describe file('/var/git_test/master/description.txt') do
+  		it { should be_a_file }
+  		its(:content) { should match /master/ }
+  	end
+
   end
+
+  context 'using resource develop' do
+
+  	describe file('/var/git_test/develop/description.txt') do
+  		it { should be_a_file }
+  		its(:content) { should match /develop/ }
+  	end
+
+  end
+
+  context 'using resource develop and checkout_branch' do
+
+  	describe file ('/var/git_test/develop_to_local/description.txt') do 
+  		it { should be_a_file }
+  		its(:content) { should match /develop/ }
+  	end
+
+  	# Need to figure out how to "query" the local git repo for the name of the branch
+
+  	
 end
